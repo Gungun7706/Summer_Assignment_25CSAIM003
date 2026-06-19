7095e553c5536954950b0ee8fcbeade657f84164
Gungun7706 <gungun.agrawal2025@glbajajgroup.org>
2026-06-19 10:09:58 +0530

    Day-11 uploaded

---
+++ Day-11/Q-3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int prime(int a){
+    if(a<2) return 0;
+    for(int i=2;i<=a/2;i++)
+   { if(a%i==0)
+    return 0;
+   }
+   return 1;
+
+}
+
+int main(){
+
+    int a;
+    scanf("%d",&a);
+
+    printf("%d",prime(a));
+
+    return 0;
+    
+}
